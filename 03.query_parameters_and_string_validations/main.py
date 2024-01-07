b4a00dba056cee0fabe524e3e0103aa30d3331ff
Ricardo Cepeda Raza <ricardo.cepeda.raza@gmail.com>
2024-01-07 18:00:30 -0500

    query_parameters_and_numeric_validations

--- 03.query_parameters_and_string_validations/main.py
+++ 03.query_parameters_and_string_validations/main.py
@@ -1,3 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated # Annotated is used to add metadata to your parameters
 
 app = FastAPI()
+
+
+# additional validation
+# you can define a regular expression pattern that the parameter should match
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=10, min_length=2, pattern="^A")] = ...): # Ellipsis declares that the value is required
+    result = {"items": [
+        {"item_id": "Foo"},
+        {"item_id": "Bar"}
+    ]}
+    if q:
+        result.update({"q": q})
+    return result
+
+# alias parameters
+@app.get("/products/")
+async def read_products(q: Annotated[list[str] | None, Query(
+    title="Query list of strings",
+    description="Query string for the items to search in the database that have a good match",
+    alias="item-query"
+    )] = ["Ricardo", "Cepeda", "Raza"]):
+    
+    query_items = {"q": q}
+    return query_items
+
+
